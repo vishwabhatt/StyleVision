93563c10b0d7e647d3b66e527595a60fd8f022bc
Ivan <ivanism90@yahoo.com>
2024-11-30 19:23:02 -0800

    Update cnn-model.py

--- cnn-model.py
+++ cnn-model.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator  
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from sklearn.metrics import classification_report
 
 import pandas as pd
 import zipfile
